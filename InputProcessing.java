9974326c961930539c254e93b644157a451d6ba6
reena-katrix <38385155+reena-katrix@users.noreply.github.com>
2019-06-06 12:44:59 +0530

    Update InputProcessing.java

--- InputProcessing.java
+++ InputProcessing.java
@@ -4,65 +4,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.Collection;
 import java.util.Enumeration;
 
 public class InputProcessing {
-	// to extract folder contents
-	public static String unZip(String destinationDir, String FolderPath) throws IOException {
-		File file = new File(FolderPath);
-		JarFile jar = new JarFile(file);
-
-		// fist get all directories,
-		// then make those directory on the destination Path
-		String fileName = " ";
-		for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements();) {
-			JarEntry entry = (JarEntry) enums.nextElement();
-
-			fileName = destinationDir + File.separator + entry.getName();
-			File f = new File(fileName);
-
-			if (fileName.endsWith("/")) {
-				f.mkdirs();
-			}
-
-		}
-
-		// now create all files
-		for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements();) {
-			JarEntry entry = (JarEntry) enums.nextElement();
-
-			String fileName2 = destinationDir + File.separator + entry.getName();
-			File f = new File(fileName2);
-
-			if (!fileName2.endsWith("/")) {
-				InputStream is = jar.getInputStream(entry);
-				FileOutputStream fos = new FileOutputStream(f);
-
-				// write contents of 'is' to 'fos'
-				while (is.available() > 0) {
-					// System.out.println(is.toString());
-					fos.write(is.read());
-				}
-
-				fos.close();
-				is.close();
-			}
-		}
-		return fileName; // returns extracted folder
-	}
 
 	public static void ExtractJAR(String destinationDir, String FolderPath, String folderName) throws IOException {
 		File file = new File(FolderPath);
 		JarFile jar = new JarFile(file);
 		String fileName, fileName2 = " ";
-		// fist get all directories,
-		// then make those directory on the destination Path
 		for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements();) {
 			JarEntry entry = (JarEntry) enums.nextElement();
-			// fileName = " ";
 			fileName = destinationDir + File.separator + entry.getName();
 			File f = new File(fileName);
-
 			if (fileName.endsWith("/")) {
 				f.mkdirs();
 			}
@@ -73,13 +27,10 @@ public class InputProcessing {
 			fileName2 = " ";
 			fileName2 = destinationDir + File.separator + entry.getName();
 			File f = new File(fileName2);
-
 			if (!fileName2.endsWith("/") && entry.getName().indexOf(folderName) == 0
 					&& entry.getName().endsWith("xml")) {
 				InputStream is = jar.getInputStream(entry);
 				FileOutputStream fos = new FileOutputStream(f);
-
-				// write contents of 'is' to 'fos'
 				while (is.available() > 0) {
 					fos.write(is.read());
 				}
@@ -88,6 +39,22 @@ public class InputProcessing {
 				is.close();
 			}
 		}
-		// return fileName2;
+	}
+
+	public static void listFilesForFolder(final File folder) throws IOException {
+		System.out.println(folder.length());
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.getAbsolutePath().endsWith("jar")) {
+				System.out.println(fileEntry.getAbsolutePath());
+				ExtractJAR("C:\\Users\\reenaya\\Music", fileEntry.getAbsolutePath(), "hibernate");
+
+			}
+		}
+	}
+
+	public static void ExtractFolder(File FolderPath) throws IOException {
+
+		listFilesForFolder(FolderPath);
+
 	}
 }
