c93da209c035580baf06c2616ba1669789a06a72
reena-katrix <38385155+reena-katrix@users.noreply.github.com>
2019-06-07 15:59:39 +0530

    Update Comparator.java

--- Comparator.java
+++ Comparator.java
@@ -7,12 +7,12 @@ import java.util.List;
 
 public class Comparator {
 
-	void AddedDeleted(HashMap<String, String> h1, HashMap<String, String> h2) {
+	void AddedDeleted(HashMap<String, String> h1, HashMap<String, String> h2, String col) {
 		Iterator iter = h1.entrySet().iterator();
 		while (iter.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter.next();
 			if (h2.get(entry.getKey()) == null)
-				System.out.println("removed attribues: " + entry.getKey() + "=" + entry.getValue());
+				System.out.println(col + ": " + "removed attribues: " + entry.getKey() + "=" + entry.getValue());
 
 		}
 
@@ -21,19 +21,19 @@ public class Comparator {
 		while (iter2.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter2.next();
 			if (h1.get(entry.getKey()) == null)
-				System.out.println("added attributes :" + entry.getKey() + " =" + entry.getValue());
+				System.out.println(col + ": " + "added attributes :" + entry.getKey() + " =" + entry.getValue());
 
 		}
 	}
 
-	void updatedPoints(HashMap<String, String> h1, HashMap<String, String> h2) {
+	void updatedPoints(HashMap<String, String> h1, HashMap<String, String> h2, String col) {
 		Iterator iter = h1.entrySet().iterator();
 		while (iter.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter.next();
 			if (h2.get(entry.getKey()) != null) {
 				if (!entry.getValue().equals(h2.get(entry.getKey())))
-					System.out.println(entry.getKey() + "=" + entry.getValue() + " is changed to " + entry.getKey()
-							+ "=" + h2.get(entry.getKey()));
+					System.out.println(col + " :" + entry.getKey() + "=" + entry.getValue() + " is changed to "
+							+ entry.getKey() + "=" + h2.get(entry.getKey()));
 			}
 		}
 	}
@@ -42,20 +42,20 @@ public class Comparator {
 			ArrayList<String> c1, ArrayList<String> c2) {
 
 		// System.out.println(Col2);
-		System.out.println("--------------Removed Columns-------------");
+		// System.out.println("--------------Removed Columns-------------");
 		for (int i = 0; i < c1.size(); i++) {
 			if (c2.contains(c1.get(i)) == false) {
 				System.out.println(c1.get(i) + " " + " column is Removed");
 			}
 		}
-		System.out.println("--------------Added Columns-------------");
+		// System.out.println("--------------Added Columns-------------");
 		for (int i = 0; i < c2.size(); i++) {
 			if (c1.contains(c2.get(i)) == false) {
 				System.out.println(c2.get(i) + " " + " column is Added");
 			}
 		}
 
-		System.out.println("--------------Updated Columns-------------");
+		// System.out.println("--------------Updated Columns-------------");
 		for (int i = 0; i < c1.size(); i++) {
 			if (c2.contains(c1.get(i))) // if column is in list2
 			{
@@ -93,22 +93,23 @@ public class Comparator {
 				// now we have indexes to compare details at them.
 				ArrayList<HashMap<String, String>> comp1 = list1.get(index1);
 				ArrayList<HashMap<String, String>> comp2 = list2.get(index2);
-				System.out.println();
-				System.out.println(c1.get(i) + " is updated with following changes:");
-
+				// System.out.println(c1.get(i) + " is updated with following
+				// changes:");
+				String col = c1.get(i);
 				HashMap<String, String> colm1 = comp1.get(0);
 				HashMap<String, String> colm2 = comp2.get(0);
-				System.out.println("changes in column :");
-				AddedDeleted(colm1, colm2);
-				updatedPoints(colm1, colm2);
+				// System.out.println("changes in column :");
+				AddedDeleted(colm1, colm2, col);
+				updatedPoints(colm1, colm2, col);
 
-				System.out.println("changes in column objetcs:");
+				// System.out.println("changes in column objetcs:");
 				for (int p = 1; p < comp1.size(); p++) {
 					for (int s = 1; s < comp2.size(); s++) {
 						HashMap<String, String> change1 = comp1.get(p);
 						HashMap<String, String> change2 = comp2.get(s);
-						AddedDeleted(change1, change2);
-						updatedPoints(change1, change2);
+						String column = c1.get(i) + "\tReferences";
+						AddedDeleted(change1, change2, column);
+						updatedPoints(change1, change2, column);
 
 					}
 				}
