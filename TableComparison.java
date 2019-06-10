ebd9da61a347530d4249dec2c602329dfcbe4f89
reena-katrix <38385155+reena-katrix@users.noreply.github.com>
2019-06-10 12:37:10 +0530

    Update TableComparison.java

--- TableComparison.java
+++ TableComparison.java
@@ -9,28 +9,26 @@ import java.io.*;
 
 public class TableComparison {
 
-	static String OldVersion = "OldVersion"; // give location of folders to be
-												// created
-	static String NewVersion = "NewVersion";
-
+	static String OldVersion = "ExtractedEPNM0";          //give location of folders to be created
+	static String NewVersion = "ExtractedEPNM1";
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		InputProcessing input = new InputProcessing();
-		// 2 folders as input
+		//2 folders as input
 		String oldFolderInput = br.readLine();
 		String newFolderInput = br.readLine();
 
-		File epnm0 = new File(oldFolderInput); // folder 1 in file format
-		File epnm1 = new File(newFolderInput); // folder 2 in file format
-
-		// making old and new folders to store all new and old jar extracts
+		File Version1 = new File(oldFolderInput); // folder 1 in file format
+		File Version2 = new File(newFolderInput); // folder 2 in file format
+		
+		//making old and new folders to store all new and old jar extracts
 		new File(OldVersion).mkdirs();
 		new File(NewVersion).mkdirs();
 
-		// extract jar into above folders
-		input.ExtractFolder(epnm0, OldVersion); // src,dest
-		input.ExtractFolder(epnm1, NewVersion); // src,dest
+		//extract jar into above folders
+		input.ExtractFolder(Version1, OldVersion);           //src,dest
+		input.ExtractFolder(Version2, NewVersion);           //src,dest
 
 		// ----------------------------Comparing two versions-------------------
 		File dir1 = new File("OldVersion");
