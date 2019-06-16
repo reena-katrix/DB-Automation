1ff38bb59a239969380489c317572f2ca965f614
reena-katrix <38385155+reena-katrix@users.noreply.github.com>
2019-06-16 19:15:07 +0530

    Update CompareVersions.java

--- CompareVersions.java
+++ CompareVersions.java
@@ -1,3 +1,4 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,13 +14,15 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
-public class CompareVersions {
+public class CompareVersion {
+
 	public void getDiff(File dirA, File dirB) throws ParserConfigurationException, SAXException, IOException {
+		// System.out.println(dirA.getName()+" "+dirB.getName()+"\tDIRECTORY");
 		File[] fileList1 = dirA.listFiles();
 		File[] fileList2 = dirB.listFiles();
 		Arrays.sort(fileList1);
 		Arrays.sort(fileList2);
-		HashMap<String, File> map1; // name,
+		HashMap<String, File> map1; // name
 		if (fileList1.length < fileList2.length) {
 			map1 = new HashMap<String, File>();
 			for (int i = 0; i < fileList1.length; i++) {
@@ -29,19 +32,20 @@ public class CompareVersions {
 			compareNow(fileList2, map1);
 		} else // older<newer
 		{
-			// System.out.println("fileList2.length <= fileList1.length" +
-			// fileList2.length + " " + fileList1.length);
 			map1 = new HashMap<String, File>();
 			for (int i = 0; i < fileList2.length; i++) {
 				map1.put(fileList2[i].getName(), fileList2[i]); // name,location
 																// of file
 			}
+
 			compareNow(fileList1, map1);
 		}
+
 	}
 
 	public void compareNow(File[] fileArr, HashMap<String, File> map)
 			throws ParserConfigurationException, SAXException, IOException {
+
 		for (int i = 0; i < fileArr.length; i++) {
 			String fName = fileArr[i].getName();
 			File fComp = map.get(fName);
@@ -53,33 +57,35 @@ public class CompareVersions {
 					getDiff(fileArr[i], fComp);
 				} else // else check if same or diff contents
 				{
+					// System.out.println(fileArr[i]+" "+ fComp);
+
 					String cSum1 = checksum(fileArr[i]);
 					String cSum2 = checksum(fComp);
 					// System.out.println("checksums:"+cSum1+" "+cSum2);
 					if (!cSum1.equals(cSum2)) {
-						System.out.println(fileArr[i].getName() + "\t at \t" + fileArr[i].getParent()
-								+ "\t" + "different from \t" + " " + fComp.getName() + "\t" + "at \t"
-								+ fComp.getParent());
-						// add code to pass these two files to check for points
-						// of difference
+
 						CompareFiles compare = new CompareFiles();
-						compare.CompareFileContents(fileArr[i], fComp);
-						System.out.println();
+						compare.CompareFileContent(fileArr[i], fComp);
 					} else {
-//						System.out.println(fileArr[i].getName() + "\t at location:\t" + fileArr[i].getAbsolutePath()
-//								+ "\t identical to " + "\t" + fComp.getName() + "\t" + "at location:\t"
-//								+ fComp.getAbsolutePath());
+						// fileArr[i].delete();
+						// TablesIdentical.add(fileArr[i].getName());
 					}
 				}
 			} else // if shorter file not found in bigger one
 			{
 				if (fileArr[i].isDirectory()) // check if old is directory
 				{
-					traverseDirectory(fileArr[i]); // if yes traverse it
+					traverseDirectory(fileArr[i]); // it
 				} else // if not then old is not in newer
 				{
-					System.out.println(fileArr[i].getName() + "\t" + "only in " + fileArr[i].getParent());
-					System.out.println();
+					// System.out.println(fileArr[i].getName()+"\tdeletedadded");
+					// System.out.println(fileArr[i].getName() + "\t" + "only in
+					// " + fileArr[i].getParent());
+					if (fileArr[i].getParent().contains("ExtractedEPNM0") && fileArr[i].getName().contains("hbm.xml"))
+						TableComparison.TablesDeleted.add(fileArr[i].getName());
+					else if (fileArr[i].getParent().contains("ExtractedEPNM1")
+							&& fileArr[i].getParent().contains("hbm.xml"))
+						TableComparison.TablesAdded.add(fileArr[i].getName());
 				}
 			}
 		}
@@ -93,20 +99,31 @@ public class CompareVersions {
 				traverseDirectory(fileFrmMap);
 			} else // only in newer
 			{
-				System.out.println(fileFrmMap.getName() + "\t" + "only in " + fileFrmMap.getParent());
-				System.out.println();
+				// System.out.println(fileFrmMap.getName()+"\tdeletedadded");
+				// System.out.println(fileFrmMap.getName() + "\t" + "only in " +
+				// fileFrmMap.getParent());
+				if (fileFrmMap.getParent().contains("ExtractedEPNM0") && fileFrmMap.getName().contains("hbm.xml"))
+					TableComparison.TablesDeleted.add(fileFrmMap.getName());
+				else if (fileFrmMap.getParent().contains("ExtractedEPNM1") && fileFrmMap.getName().contains("hbm.xml"))
+					TableComparison.TablesAdded.add(fileFrmMap.getName());
 			}
 		}
 	}
 
-	public void traverseDirectory(File dir) {
+	public void traverseDirectory(File dir) throws IOException {
 		File[] list = dir.listFiles();
 		for (int k = 0; k < list.length; k++) {
 			if (list[k].isDirectory()) {
 				traverseDirectory(list[k]);
 			} else {
-				System.out.println(list[k].getName() + "\t" + "only in " + list[k].getParent());
-				System.out.println();
+				// System.out.println(list[k].getName()+"\tdeletedadded");
+
+				// System.out.println(list[k].getName() + "\t" + "only in " +
+				// list[k].getParent());
+				if (list[k].getParent().contains("ExtractedEPNM0") && list[k].getName().contains("hbm.xml"))
+					TableComparison.TablesDeleted.add(list[k].getName());
+				else if (list[k].getParent().contains("ExtractedEPNM1") && list[k].getName().contains("hbm.xml"))
+					TableComparison.TablesAdded.add(list[k].getName());
 			}
 		}
 	}
@@ -135,4 +152,5 @@ public class CompareVersions {
 			return null;
 		}
 	}
+
 }
