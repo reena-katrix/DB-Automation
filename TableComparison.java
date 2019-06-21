20c97c3e5495019ea13e00943f9106751ba9eeb6
reena-katrix <38385155+reena-katrix@users.noreply.github.com>
2019-06-21 16:23:08 +0530

    Update TableComparison.java

--- TableComparison.java
+++ TableComparison.java
@@ -11,6 +11,7 @@ import javax.script.*;
 
 //traversed info
 //https://paste.ubuntu.com/p/Mcy5NHJrhb/
+//prints good html report as in finalHTMLreport.pdf
 public class TableComparison {
 
 	// -------------------to store--------------
@@ -27,6 +28,9 @@ public class TableComparison {
 													// be created
 	static String NewVersion = "ExtractedEPNM1";
 
+	File dir1 = new File("ExtractedEPNM0");
+	File dir2 = new File("ExtractedEPNM1");
+
 	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 
 		File f = new File("myhtml.html");
@@ -37,7 +41,7 @@ public class TableComparison {
 		// 2 folders as input
 		String oldFolderInput = br.readLine();
 		String newFolderInput = br.readLine();
-
+		
 		File Version1 = new File(oldFolderInput); // folder 1 in file format
 		File Version2 = new File(newFolderInput); // folder 2 in file format
 
@@ -52,141 +56,124 @@ public class TableComparison {
 		// ----------------------------Comparing two versions-------------------
 		File dir1 = new File(OldVersion);
 		File dir2 = new File(NewVersion);
+
 		CompareVersion compare = new CompareVersion();
 		try {
-			compare.getDiff(dir1,dir2);
-//			compare.getDiff(new File("ExtractedEPNM0"), new File("ExtractedEPNM0"));
+			compare.getDiff(dir1, dir2);
 		} catch (IOException ie) {
 			ie.printStackTrace();
 		}
 
-//		for (Entry<String, ArrayList<String>> m : newBKs.entrySet()){
-//			System.out.println(m.getKey()+":"+m.getValue());
-//		}
-//		for (Entry<String, ArrayList<String>> m : oldBKs.entrySet()){
-//			System.out.println(m.getKey()+":"+m.getValue());
-//		}
-//		to check 
-		
 		Collections.sort(TablesAdded);
 		Collections.sort(TablesDeleted);
 		bw.write("<h4>Comparision between EPNM 3.0 and EPNM 3.1</h4>");
 		if (!TablesAdded.isEmpty()) {
-			// System.out.println("ADDED TABLES:" + TablesAdded);
 			bw.write("<strong>Added Tables</strong><ol>");
 			for (int i = 0; i < TablesAdded.size(); i++) {
-				String add = TablesAdded.get(i).substring(0, TablesAdded.get(i).indexOf("."));
+				String add = TablesAdded.get(i);
 				bw.write("<li>" + add + "</li>");
 				// for listing BKs
-				if (newBKs.containsKey(add)) {
-					System.out.println("table found at:"+i+add);
-					ArrayList<String> cols = new ArrayList<>(newBKs.get(add));
-					System.out.println("addedBKs:"+cols);
-					if (!cols.isEmpty()) {
-						bw.write("<strong>BKs</strong><ol>");
-						for (int in = 0; in < cols.size(); in++)
-							bw.write("<li>" + cols.get(in) + "</li>");
-						bw.write("</ol>");
-					}
-
-				}
+				// if (newBKs.containsKey(add)) {
+				// // System.out.println("table found at:" + i + add);
+				// ArrayList<String> cols = new ArrayList<>(newBKs.get(add));
+				// // System.out.println("addedBKs:" + cols);
+				// if (!cols.isEmpty() && cols.size() > 0) {
+				// bw.write("<strong>BKs</strong><ol>");
+				// for (int in = 0; in < cols.size(); in++)
+				// bw.write("<li>" + cols.get(in) + "</li>");
+				// bw.write("</ol>");
+				// }
+				//
+				// }
 			}
 			bw.write("</ol>");
 		}
 
 		if (!TablesDeleted.isEmpty()) {
-			// System.out.println("DELETED TABLES:" + TablesDeleted);
 			bw.write("<strong>DeletedTables</strong><ol>");
 			for (int i = 0; i < TablesDeleted.size(); i++) {
-				String del = TablesDeleted.get(i).substring(0, TablesDeleted.get(i).indexOf("."));
+				String del = TablesDeleted.get(i);
 				bw.write("<li>" + del + "</li>");
 			}
 			bw.write("</ol>");
 		}
-
+		// SortedMap<String, UpdatedTableDetails>
 		if (!allUpdatedTables.isEmpty()) {
 			bw.write("<strong>UpdatedTables</strong><ol>");
 			String tableName = "";
 			for (Entry<String, UpdatedTableDetails> m : allUpdatedTables.entrySet()) {
-				tableName = m.getKey().substring(0, m.getKey().indexOf("."));
+
+				tableName = m.getKey();
 				UpdatedTableDetails obj = m.getValue();
 				if (!obj.getAddedColumns().isEmpty() || !obj.getRemovedColumns().isEmpty()
 						|| !obj.getUpdatedCol().isEmpty()) {
-					// System.out.println("UpdatedTable:" + tableName);
 					bw.write("<li>" + tableName + "</li><ul>");
-					ArrayList<String> dummy1 = null, dummy2 = null;
+					ArrayList<String> dummy1 = new ArrayList<String>();
+					ArrayList<String> dummy2 = new ArrayList<String>();
+					ArrayList<String> pro1 = new ArrayList<String>();
+					ArrayList<String> pro2 = new ArrayList<String>();
+
 					if (newBKs.containsKey(tableName)) {
 						dummy1 = newBKs.get(tableName);
+						pro1 = new ArrayList<>(dummy1);
 					}
 					if (oldBKs.containsKey(tableName)) {
 						dummy2 = oldBKs.get(tableName);
+						pro2 = new ArrayList<>(dummy2);
 					}
-					System.out.println(dummy1);
-					System.out.println(dummy2);
-					if(!dummy1.isEmpty()){
-					boolean addedBK = dummy1.removeAll(dummy2);
-					if (addedBK) {
-						bw.write("<strong>added BKs</strong><ol>");
-						for (int a = 0; a < dummy1.size(); a++)
-							bw.write("<li>" + dummy1.get(a) + "</li>");
-						bw.write("</ol>");
+					if (!dummy1.isEmpty()) {
+						boolean addedBK = dummy1.removeAll(dummy2);
+						if ((addedBK && dummy1.size() > 0)) {
+							bw.write("<li><strong>added BKs</strong></li><ol>");
+							for (int a = 0; a < dummy1.size(); a++)
+								bw.write("<li>" + dummy1.get(a) + "</li>");
+							bw.write("</ol>");
+						}
 					}
+
+					if (!pro2.isEmpty()) {
+						boolean removedBK = pro2.removeAll(pro1);
+						if ((removedBK && pro2.size() > 0)) {
+							bw.write("<li><strong>removed BKs</strong></li><ol>");
+							for (int a = 0; a < pro2.size(); a++)
+								bw.write("<li>" + pro2.get(a) + "</li>");
+							bw.write("</ol>");
+						}
 					}
-					// dummy1 = new ArrayList<>(added);
-					// dummy2 = new ArrayList<>(deleted);
-					// boolean removedBK = dummy2.removeAll(dummy1);
-					// if (removedBK) {
-					// bw.write("" + tableName + " <strong>removed
-					// BKs</strong><ol>");
-					// for (int a = 0; a < dummy2.size(); a++)
-					// bw.write("<li>" + dummy2.get(a) + "</li>");
-					// bw.write("</ol>");
-					// }
 
 					if (!obj.getAddedColumns().isEmpty()) {
-						// System.out.println("addedCol:");
 						bw.write("<li>ColumnsAdded</li><ol>");
 						for (int i = 0; i < obj.getAddedColumns().size(); i++) {
-							// System.out.println(obj.getAddedColumns().get(i));
 							bw.write("<li>" + obj.getAddedColumns().get(i) + "</li>");
 						}
 						bw.write("</ol>");
 
 					}
 					if (!obj.getRemovedColumns().isEmpty()) {
-						// System.out.println("removedCol:");
 						bw.write("<li>ColumnsRemoved</li><ol>");
 						for (int i = 0; i < obj.getRemovedColumns().size(); i++) {
-							// System.out.println(obj.getRemovedColumns().get(i));
 							bw.write("<li>" + obj.getRemovedColumns().get(i) + "</li>");
 						}
 						bw.write("</ol>");
 					}
 
-					if (!obj.getUpdatedCol().isEmpty()) {
-						// System.out.println("updatedCol:");
+					if (!obj.getUpdatedCol().isEmpty()) { // araylist of
+															// hashmaps
 						bw.write("<li>ColumnsUpdated</li><ol>");
-						for (int i = 0; i < obj.getUpdatedCol().size(); i++) { // total
-																				// updated
-																				// col
+						for (int i = 0; i < obj.getUpdatedCol().size(); i++) {
 							HashMap<String, String> ok = obj.getUpdatedCol().get(i);
-							// System.out.println(ok);
 							for (Entry<String, String> map : ok.entrySet()) {
-								// System.out.println(map.getKey() + " " +
-								// map.getValue());
-								bw.write("<li>" + map.getKey() + "&nbsp" + map.getValue() + "</li>");
+								bw.write("<li>" + map.getKey() + "</li>");
+								bw.write("" + map.getValue() + "");
 							}
 						}
 						bw.write("</ol>");
-
 					}
 					bw.write("</ul>");
-					// System.out.println();
 				}
 			}
-
+			bw.write("</ol>");
 		}
-
 		bw.close();
 	}
 
